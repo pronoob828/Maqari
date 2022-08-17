2026845af76a94538bac297b190e55389aa902ad
unknown <pronoob12@tutanota.com>
2022-08-17 23:58:16 +0300

    debug false

--- final_project/settings.py
+++ final_project/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e$7x)h&(mmqyf_)zhw*#8@b+20&qrxk=cp_q1lmoiz$e*fuz8q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','192.168.1.6','.herokuapp.com']
 
